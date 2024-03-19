47b57ca42b95c7b4d95dc50b31134079353ccecb
domenicog03 <domenico.galeone.info@gmail.com>
2024-03-19 19:11:15 +0100

    feat: add alert number in square

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const play = document.querySelector('#play-btn');
 play.addEventListener('click',function(){
     const mainGrid = document.querySelector('#grid');
+    mainGrid.innerHTML="";
     for(let i = 1; i <= 100; i++) {
                 const square = generateSquare(i);
     
@@ -19,9 +20,14 @@ play.addEventListener('click',function(){
         const newSquare = document.createElement('div');
         newSquare.classList.add('square');
         newSquare.innerHTML = `<span>${number}</span>`;
+        newSquare.addEventListener('click', function() {
+           // console.log(document.querySelector('#grid').value);
+           alert(number);
+
+        });
         return newSquare;
     }
 
-        mainGrid.addEventListener('click', function() {
-            console.log(document.querySelector('#grid').value);
-        });
+      //  mainGrid.addEventListener('click', function() {
+      //      console.log(document.querySelector('#grid').value);
+      //  });
